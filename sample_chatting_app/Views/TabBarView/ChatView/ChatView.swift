3207d1dbe910a8bdfc676fd1a2e561c02fe2458a
Manisankar7 <manisankarsarma.chittela@softsuave.org>
2024-03-02 19:52:10 +0530

    - Tab bar and Message screen.

---
+++ sample_chatting_app/Views/TabBarView/ChatView/ChatView.swift
@@ -0,0 +1,45 @@
+//
+//  ChatView.swift
+//  sample_chatting_app
+//
+//  Created by Softsuave on 02/03/24.
+//
+
+import SwiftUI
+
+struct ChatView: View {
+    var body: some View {
+        ZStack(alignment: .top) {
+            Color.splashScreenGradient
+            HStack() {
+                Image(.searchBarIcon)
+                    .resizable()
+                    .aspectRatio(contentMode: .fit)
+                    .frame(width: 40, height: 40)
+                    .padding(EdgeInsets(top: 10, leading: 10, bottom: 0, trailing: 0))
+                Spacer()
+                Text("Home")
+                    .font(.title)
+                    .padding(.top, 10)
+                    .foregroundColor(.white)
+                Spacer()
+                Image(systemName: "person.circle.fill")
+                    .resizable()
+                    .aspectRatio(contentMode: .fit)
+                    .padding(.top, 10)
+                    .frame(width: 40, height: 40)
+
+            
+               
+               
+            }
+            .frame(width: .infinity, height: 50, alignment: .top)
+            
+        }
+        
+    }
+}
+
+#Preview {
+    ChatView()
+}
